6f1f6660c46695e46a3ad99427c1cb56a41819d0
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2023-02-19 23:15:15 +0000

    receipt model str

--- backend/receipts/models.py
+++ backend/receipts/models.py
@@ -24,3 +24,5 @@ class Receipt(models.Model):
     iban = models.CharField(max_length=255, null=True)
     reimbursed = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.date}: {self.username} - {self.reason} - {self.total_amount}'
